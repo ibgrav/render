90f5b7ab67137b58b6816e2a011ee3136577dd30
ibgrav <ibgrav@gmail.com>
2021-08-25 09:22:44 -0700

    switch order

--- server/components/page.ts
+++ server/components/page.ts
@@ -3,10 +3,10 @@ import { Component } from "./Component";
 export class Page extends Component {
   render(state: Record<string, any>) {
     return this.html`
-      ${this.vue({ id: "one", name: "one", props: { message: "hi 1" } })}
       <h2>This is SSR html. Change 2</h2>
-      ${this.vue({ id: "two", name: "two", props: { message: "hi 2" } })}
       <pre>${JSON.stringify(state, null, 2)}</pre>
+      ${this.vue({ id: "one", name: "one", props: { message: "hi 1" } })}
+      ${this.vue({ id: "two", name: "two", props: { message: "hi 2" } })}
       ${this.vue({ id: "three", name: "three", props: { message: "hi another" } })}
     `;
   }
